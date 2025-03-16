b3a796893faaf9ce3fb839d708d50a40525f0ab6
Talal-Hassan-Programmer <Talal-Hassan@outlook.com>
2025-03-17 02:35:53 +0300

    Writing the main code in main.py file for starting

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+from discord import app_commands
+
+
+#MAIN CODE FROM DISCORD DOCS
+MY_GUILD = discord.Object(id=1332629729118785629)  # replace with your guild id
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+
+#SETTING THE BOT AND INTENTS
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
+
+#ON READY EVENT MAIN EVENT
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print('------')
+
+#COMMANDS  (hello)
+@client.tree.command()
+async def hello(interaction: discord.Interaction):
+    """Says hello!"""
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+
+
+
+#RUNNING THE BOT
+client.run("MTMzNzcwNjM0MDkxNDY5MjEzNg.GA-VDY.PyzYKcPnbpIAi8NjgXOFY_dCuOeIWcUivvKkoc")
